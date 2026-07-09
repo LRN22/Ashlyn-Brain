644562bd7af5b8c1e931ba4d05f127b291104ae4
LRN22 <lynn.r.nichols@gmail.com>
2026-07-09 14:14:10 -0500

    Replace face with anime Moof and add talk bob
    - Generate kid-friendly anime orange tabby avatar from MOOF.jpeg
    - Use bounce/glow speaking animation (no fake mouth overlay)
    - Update prompts, greetings, icons, SW cache v3

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// Ashlyn Brain Service Worker - enables offline shell + installable PWA
-const CACHE_NAME = "ashlyn-brain-v2";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./ashlyn-face.png"];
+// Ashlyn Brain Service Worker - offline shell + installable PWA
+const CACHE_NAME = "ashlyn-brain-v3";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./icon.png", "./ashlyn-face.png"];
 
 self.addEventListener("install", (e) => {
     self.skipWaiting();
